cbdb1c5939f43d1a1568476690a9a458599215ed
chengyuezhen <807129279@qq.com>
2022-05-04 10:52:01 +0800

    second commit

--- src/main/java/com/cyz/git/GitTest.java
+++ src/main/java/com/cyz/git/GitTest.java
@@ -3,5 +3,6 @@ package com.cyz.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git222!!!");
     }
 }
